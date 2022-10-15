8bca3a888f5cf9ee1291c8d9ca48cd0f8f0eba00
remusris <91991249+remusris@users.noreply.github.com>
2022-10-15 11:50:41 -0700

    first valid response!

--- background.js
+++ background.js
@@ -1,4 +1,26 @@
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-    console.log(response.farewell);
-  });
+// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+//     console.log(response.farewell);
+//   });
   
+
+// chrome.runtime.sendMessage({
+//   msg: "something_completed", 
+//   data: {
+//       subject: "Loading",
+//       content: "Just completed!"
+//   }
+// });
+
+// chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+//   //alert(message.data);
+//     chrome.runtime.sendMessage({data: "hello"},function(response){
+//       console.log(message);
+//         });
+//         });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.method == "getStatus") {
+      console.log(request.data)
+      sendResponse({ method: "peepee", data: "poopoo" })
+  }
+});
--- popup.js
+++ popup.js
@@ -1,9 +1,34 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting === "hello")
-        sendResponse({farewell: "goodbye"});
-    }
-  );
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       if (request.greeting === "hello")
+//         sendResponse({farewell: "goodbye"});
+//     }
+//   );
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//       if (request.msg === "something_completed") {
+//           //  To do something
+//           console.log(request.data.subject)
+//           console.log(request.data.content)
+//       }
+//   }
+// );
+
+
+// chrome.runtime.sendMessage({data:"Handshake"},function(response){
+	
+// });
+// chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+// str = JSON.stringify(message.data);
+// console.log(message);
+// });
+
+chrome.runtime.sendMessage({ method: "getStatus", data: "xxx" }, function (res) {
+  // document.getElementById("popupElement1").innerText = res.method;
+  // document.getElementById("popupElement2").innerText = res.data;
+  console.log(res.method);
+  console.log(res.data);
+
+return true;
+});
